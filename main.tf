e358f269887fa8a935578bbb14ed7cbb2089b8b5
Veselin Filipov <veselinfilipov01@gmail.com>
2026-04-27 13:48:18 +0300

    Initial Terraform configuration with Azure AD auth

---
+++ main.tf
@@ -0,0 +1,98 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.66.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.8.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "hmstorage2026"
+    container_name       = "homiescontainer"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+  subscription_id = "45ab7c0b-0483-4cfa-b5bb-498a103b8661"
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = "${var.resource_group_name}-${random_integer.ri.result}"
+  location = "swedencentral"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_mssql_server" "ams" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11!23" # Твоята статична парола
+  minimum_tls_version          = "1.2"
+}
+
+resource "azurerm_mssql_database" "amd" {
+  name                 = var.sql_database_name
+  server_id            = azurerm_mssql_server.ams.id
+  collation            = "SQL_Latin1_General_CP1_CI_AS"
+  license_type         = "LicenseIncluded"
+  max_size_gb          = 2
+  sku_name             = "S0"
+  zone_redundant       = false
+  storage_account_type = "Local"
+}
+
+resource "azurerm_mssql_firewall_rule" "firewallrule" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.ams.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.ams.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.amd.name};User ID=${azurerm_mssql_server.ams.administrator_login};Password=thisIsKat11!23;Trusted_Connection=False;MultipleActiveResultSets=True;Encrypt=True;"
+  }
+}
+
+resource "azurerm_app_service_source_control" "assc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.github_repository_url
+  branch                 = "main"
+  use_manual_integration = false
+}
---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+  default     = "rg-homies"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "swedencentral"
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "App Service Plan name"
+  default     = "asp-homies"
+}
+
+variable "web_app_name" {
+  type        = string
+  description = "Web App name"
+  default     = "homies-app"
+}
+
+variable "sql_server_name" {
+  type        = string
+  description = "SQL Server name"
+  default     = "sqlserver-homies"
+}
+
+variable "sql_admin_username" {
+  type        = string
+  description = "SQL Server admin username"
+  default     = "missadministrator"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "SQL Server admin password"
+  sensitive   = true
+  default     = "thisIsKat11!23" # ← Добави това, ако искаш
+}
+
+variable "sql_database_name" {
+  type        = string
+  description = "SQL Database name"
+  default     = "Homies"
+}
+
+variable "firewall_rule_name" {
+  type        = string
+  description = "Firewall rule name"
+  default     = "AllowAllAzureServices"
+}
+
+variable "github_repository_url" {
+  type        = string
+  description = "GitHub repository URL"
+}
