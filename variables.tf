bc35bcb564c871cae425fa3a88629a0e127d52ff
Veselin Filipov <veselinfilipov01@gmail.com>
2026-04-28 14:21:18 +0300

    change region to northeurope

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   type        = string
   description = "Azure region"
-  default     = "francecentral"
+  default     = "northeurope"
 }
 
 variable "app_service_plan_name" {
